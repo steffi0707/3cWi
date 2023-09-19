01e51a5058703897e956bcff4c0bd76bd0cb9ba7
Steffi <stefanie.trautz@gmail.com>
2023-09-19 09:30:35 +0200

    finishedIfBedingungen2

--- src/at/steffi/IfBedingung1.java
+++ src/at/steffi/IfBedingung1.java
@@ -1,2 +1,19 @@
-package at.steffi;public class IfBedingung1 {
+package at.steffi;
+import java.util.Random;
+public class IfBedingung1 {
+    public static void main(String[] args) {
+        //Random Number between 0 and 100
+        Random random = new Random();
+        int randomNumber = random.nextInt(100);
+        System.out.println(randomNumber);
+        if (randomNumber<20){
+            System.out.println("Mini");
+        }
+        else if (randomNumber>50) {
+            System.out.println("Large");
+        }
+        else {
+            System.out.println("Medium");
+        }
+    }
 }
