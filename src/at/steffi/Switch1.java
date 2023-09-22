531da6a4907444ecd3aab053e2cb027cd7acd8c4
Steffi <stefanie.trautz@gmail.com>
2023-09-22 08:25:45 +0200

    finished Switch1

--- src/at/steffi/Switch1.java
+++ src/at/steffi/Switch1.java
@@ -4,6 +4,25 @@ import java.util.Random;
 public class Switch1 {
     public static void main(String[] args) {
         Random random = new Random();
-        int randomNumber1 = random.nextInt(5-10);
+        int randomNumber1 = random.nextInt(5,10);
+        switch (randomNumber1) {
+            case 10:
+                System.out.println("Ten");
+                break;
+            case 9:
+                System.out.println("Nine");
+                break;
+            case 8:
+                System.out.println("Eight");
+                break;
+            case 7:
+                System.out.println("Seven");
+                break;
+            case 6:
+                System.out.println("Six");
+                break;
+            case 5:
+                System.out.println("Five");
+        }
     }
 }
