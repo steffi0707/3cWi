85b32e36a1839e05b402bb2bfab1fa500e9cc45d
Steffi <stefanie.trautz@gmail.com>
2023-09-22 09:33:01 +0200

    worked on While1

--- src/at/steffi/While1.java
+++ src/at/steffi/While1.java
@@ -1,7 +1,13 @@
 package at.steffi;
 
+import java.util.Random;
+
 public class While1 {
     public static void main(String[] args) {
+        Random random = new Random();
+        int randomNumber1 = random.nextInt(10,30);
+        int randomNumber2 = random.nextInt(10,30);
+
 
     }
 }
