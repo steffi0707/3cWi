b3c147c915c9a520d99e9123c29f3214a24620bd
Steffi <stefanie.trautz@gmail.com>
2023-09-22 08:43:07 +0200

    finished Loops1

---
+++ src/at/steffi/Loops1.java
@@ -0,0 +1,12 @@
+package at.steffi;
+
+public class Loops1 {
+    public static void main(String[] args) {
+        int katho = 0;
+        for (int i = 0; i <= 100; i++) {
+            System.out.println(i);
+            katho += i;
+            System.out.println(katho);
+        }
+    }
+}
