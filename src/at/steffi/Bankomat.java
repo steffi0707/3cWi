b2b183d9ea20787c16ccca330ec580549d51d032
Steffi <stefanie.trautz@gmail.com>
2023-09-26 09:30:10 +0200

    started Bankomat

---
+++ src/at/steffi/Bankomat.java
@@ -0,0 +1,18 @@
+package at.steffi;
+import java.util.Random;
+public class Bankomat {
+    public static void main(String[] args) {
+        int balance = 0;
+        boolean isFinished = false;
+
+        while (!isFinished) {
+            System.out.println("1. Einzahlen");
+            System.out.println("2. Abheben");
+            System.out.println("3. Kontostand");
+            System.out.println("4. Beenden");
+
+            System.out.println("Wähle eine Eingabe");
+        }
+    }
+}
+
