8c72b8e79b66f61316535633a1e8f18872a522fc
Steffi <stefanie.trautz@gmail.com>
2023-09-29 08:51:46 +0200

    finished Bankomat

--- src/at/steffi/Bankomat.java
+++ src/at/steffi/Bankomat.java
@@ -1,17 +1,44 @@
 package at.steffi;
-import java.util.Random;
+import java.util.Scanner;
+
 public class Bankomat {
     public static void main(String[] args) {
         int balance = 0;
-        boolean isFinished = false;
+        boolean isFinished = true;
+        Scanner scanner =new Scanner(System.in);
+        int selection = scanner.nextInt();
 
-        while (!isFinished) {
+        while (isFinished) {
             System.out.println("1. Einzahlen");
             System.out.println("2. Abheben");
             System.out.println("3. Kontostand");
             System.out.println("4. Beenden");
 
             System.out.println("Wähle eine Eingabe");
+            selection = scanner.nextInt();
+            System.out.println(selection);
+
+            if (selection == 1) {
+                System.out.println("Geben sie den Betrag ein den Sie einzahlen möchten");
+                selection = scanner.nextInt();
+                balance += selection;
+            }
+
+            if (selection == 2) {
+                System.out.println("Geben sie den Betrag ein den Sie Abheben möchten");
+                selection = scanner.nextInt();
+                balance -= selection;
+            }
+
+            if (selection == 3) {
+                System.out.println("Der Kontostand beträgt:" + balance);
+            }
+            if (selection == 4){
+                isFinished = false;
+                System.out.println("Machine is stopping!");
+            }
+
+
         }
     }
 }
