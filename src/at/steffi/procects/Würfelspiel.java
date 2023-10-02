4f108126ab0ab252ccfc1d1974117ded6d76e680
Steffi <stefanie.trautz@gmail.com>
2023-10-02 14:43:48 +0200

    worked on Würfelspiel

--- src/at/steffi/procects/Würfelspiel.java
+++ src/at/steffi/procects/Würfelspiel.java
@@ -3,9 +3,18 @@ import java.util.Random;
 
 public class Würfelspiel {
     public static void main(String[] args) {
+
+        boolean start = false;
+        System.out.println("Write 'start' to begin the game");
+
+        if (start == true){
         Random random = new Random();
-        int randomNumber = random.nextInt(1,7);
-        System.out.println(randomNumber);
+        int randomNumberPlayer = random.nextInt(1,7);
+        System.out.println("Player: " + randomNumberPlayer);
+
+        int randomNumberComputer = random.nextInt(1,7);
+        System.out.println("Computer: " + randomNumberComputer);
+        }
 
 
     }
