487b826aa7a78d5b997f8eb7d24e352e19baf717
Steffi <stefanie.trautz@gmail.com>
2023-10-16 13:56:01 +0200

    improved and finished CaeserVerschlüsselung

--- src/at/steffi/procects/CaeserVerschlüsselung.java
+++ src/at/steffi/procects/CaeserVerschlüsselung.java
@@ -2,8 +2,8 @@ package at.steffi.procects;
 
 public class CaeserVerschlüsselung {
     public static void main(String[] args) {
-        String[] alphabet = {" ", "a", "b", "c", "d", "e", "f", "g","h","i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s","t", "u", "v", "w", "x", "y", "z"};
-        String word = "spiel verloren";
+        String[] alphabet = {" ", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g","h","i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s","t", "u", "v", "w", "x", "y", "z"};
+        String word = "Spiel verloren";
         System.out.println("Start sentence: " + word);
         String encrypted_word = encrypt(word, alphabet, 2);
         System.out.println("Encrypted sentence: " + encrypted_word);
@@ -13,35 +13,32 @@ public class CaeserVerschlüsselung {
 
     public static String encrypt(String string, String[] cipher, int offset){
         String[] string_list = string.split("");
-        String output = "";
-        for (int i = 0; i < string_list.length; i++) {
-            String character = string_list[i];
+        StringBuilder output = new StringBuilder();
+        for (String character : string_list) {
             for (int j = 0; j < cipher.length; j++) {
                 if (character.equals(cipher[j])) {
-                    output += cipher[(j+offset)%cipher.length];
+                    output.append(cipher[(j + offset) % cipher.length]);
                 }
             }
 
         }
-        return output;
+        return output.toString();
     }
 
 
     public static String decrypt(String string, String[] cipher, int offset){
         String[] string_list = string.split("");
-        String output = "";
-        for (int i = 0; i < string_list.length; i++) {
-            String character = string_list[i];
+        StringBuilder output = new StringBuilder();
+        for (String character : string_list)
             for (int j = 0; j < cipher.length; j++) {
                 if (character.equals(cipher[j])) {
                     int new_index = j - offset;
-                    while (new_index < 0){
+                    while (new_index < 0) {
                         new_index += cipher.length;
                     }
-                    output += cipher[new_index];
+                    output.append(cipher[new_index]);
                 }
             }
-        }
-        return output;
+        return output.toString();
     }
 }
