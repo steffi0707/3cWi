e41898029b5d9921f7e2fd236380aea224ca4953
Steffi <stefanie.trautz@gmail.com>
2023-11-06 14:44:16 +0100

    worked on Tic Tac Toe

--- src/at/steffi/procects/TicTacToe.java
+++ src/at/steffi/procects/TicTacToe.java
@@ -12,7 +12,106 @@ public class TicTacToe {
     }
 
     public static void run(){
-            while (true){
+        Scanner scanner = new Scanner(System.in);
+        int[][] board = new int[3][3];
+        boolean game_is_running = true;
+
+        int player = 2;
+
+        printBoard(board);
+
+
+        while (game_is_running) {
+
+            if (player == 1) {
+                player = 2;
+            } else player = 1;
+
+            System.out.println("Player" + player + ": ");
+            int[] position = getPosition(scanner);
+            while (!checkPositionIsFree(board, position)) {
+                System.out.println("Position is already taken");
+                System.out.println("New position: ");
+                position = getPosition(scanner);
+            }
+            updateBoard(board, position, player);
+            printBoard(board);
+            if (checkBoardForWinner(board, player)) {
+                System.out.println("Player" + player + " is the winner!");
+                game_is_running = false;
+            }
+        }
+
+    }
+
+    public static void printBoard(int[][] board) {
+        for (int[] row : board) {
+            for (int state : row) {
+                if (state == 1) {
+                    System.out.print("|o");
+                } else if (state == 2) {
+                    System.out.print("|x");
+                } else {
+                    System.out.print("| ");
+                }
+            }
+            System.out.print("|");
+            System.out.println();
+        }
+    }
+
+    public static int[] getPositionFromInput(String input) {
+        String[] input_list = input.split(",");
+
+        return new int[]{Integer.parseInt(input_list[1]), Integer.parseInt(input_list[0])};
+    }
+
+    public static int[] getPosition(Scanner scanner) {
+        return getPositionFromInput(scanner.next());
+    }
+
+    public static void updateBoard(int[][] board, int[] position, int state) {
+        board[position[0]][position[1]] = state;
+    }
+
+    public static boolean checkPositionIsFree(int[][] board, int[] position) {
+        return board[position[0]][position[1]] == 0;
+    }
+
+    public static boolean checkBoardForWinner(int[][] board, int player) {
+        boolean winner = board[0][0] == player && board[0][1] == player && board[0][2] == player;
+
+        if (board[1][0] == player && board[1][1] == player && board[1][2] == player) {
+            winner = true;
+        }
+        if (board[2][0] == player && board[2][1] == player && board[2][2] == player) {
+            winner = true;
+        }
+
+
+        if (board[0][0] == player && board[1][0] == player && board[2][0] == player) {
+            winner = true;
+        }
+        if (board[0][1] == player && board[1][1] == player && board[2][1] == player) {
+            winner = true;
+        }
+        if (board[0][2] == player && board[1][2] == player && board[2][2] == player) {
+            winner = true;
+        }
+
+
+        if (board[0][0] == player && board[1][1] == player && board[2][2] == player) {
+            winner = true;
+        }
+        if (board[0][2] == player && board[1][1] == player && board[2][0] == player) {
+            winner = true;
+        }
+
+
+        return winner;
+
+
+            /*while (true){
                 if (isPlayer1){
                     System.out.println("Player1");
                 } else {
@@ -33,7 +132,9 @@ public class TicTacToe {
             }
     }
 
+
     public static boolean hasWinner(){
-        return
+        return ;*/
     }
+
 }
