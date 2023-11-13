37ec62902dc41728f363589c478810a1308824b8
Steffi <stefanie.trautz@gmail.com>
2023-11-13 14:17:35 +0100

    finished TicTacToe

--- src/at/steffi/procects/TicTacToe.java
+++ src/at/steffi/procects/TicTacToe.java
@@ -112,31 +112,6 @@ public class TicTacToe {
 
         return winner;
 
-
-            /*while (true){
-                if (isPlayer1){
-                    System.out.println("Player1");
-                } else {
-                    System.out.println("Player 2");
-                }
-                String inputOfPlayer = scanner.next();
-                String [] input = inputOfPlayer.split(",");
-                int input1 = Integer.parseInt(input[0]);
-                int input2 = Integer.parseInt(input[1]);
-                if (isPlayer1){
-                    field[input1][input2] = 1;
-                }else {
-                    field[input1][input2] = 2;
-                }
-
-                System.out.println(input1 + "-" + input2);
-                isPlayer1 = !isPlayer1;
-            }
-    }
-
-
-    public static boolean hasWinner(){
-        return ;*/
     }
 
 }
