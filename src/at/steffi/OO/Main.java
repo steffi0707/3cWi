940f047bed0be3b7fe215e4704732e78650fa90e
Steffi <stefanie.trautz@gmail.com>
2023-12-11 14:46:14 +0100

    Refactored OO Main

--- src/at/steffi/OO/Main.java
+++ src/at/steffi/OO/Main.java
@@ -9,21 +9,26 @@ public class Main {
 
         Car c1  = new Car(e1,4, "Audi", "A1234", 7, 3, "red");
         Car c2 = new Car(e2,12, "Mercedes", "M1234", 32, 5, "green");
-        Car c3 = new Car(e3,5, "Opel", "1234O", 17, 6, "blue");
+        Car c3 = new Car(e3,5, "Opel", "O1234", 17, 6, "blue");
 
-        System.out.println(c3.serialNumber);
+        //Car 1
+        System.out.println(c1.serialNumber);
+        c1.Break();
+        c1.turboBoost();
+        c1.honk(c1.amountOfRepetitions);
 
+        //Car 2
         System.out.println(c2.fuelAmount);
         c2.drive();
         System.out.println(c2.fuelAmount);
-
-        System.out.println(c1.serialNumber);
         System.out.println(c2.serialNumber);
 
-        c1.Break();
-        c1.turboBoost();
+        //Car 3
+        System.out.println(c3.serialNumber);
 
-        c1.honk(c1.amountOfRepetitions);
+
+
+
 
         c2.getRemainingRange();
 
